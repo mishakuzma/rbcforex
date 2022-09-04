28bbaebc75b32a87cc95ba1f2efe29cc827a8c58
Mykhaylo (Misha) Ivanovych Kuzma <13275599+mishakuzma@users.noreply.github.com>
2022-09-03 18:00:40 -0600

    notes for other websites

--- src/lib.rs
+++ src/lib.rs
@@ -111,8 +111,14 @@ impl BankCall {
 
 const RBC_RATES_URL: &str =
     "https://online.royalbank.com/cgi-bin/tools/foreign-exchange-calculator/rates.cgi?";
+// NOTE TD lets you pick between cash and non cash. 
+//  I should add this as a feature for CLI.
+// Also note: this one gives you all of the rates in a xml. So it would have
+//  to be parsed.
 const TD_RATES_URL: &str = 
     "https://tool.td.com/fxcal/api/fxservice/getNonCashFeed";
+// TODO Find out where CIBC makes their rates available from.
+// const CIBC_RATES_URL: &str = "";
 pub fn call_rbc(from_cur: String, to_cur: String) {
     let rbc_call = BankCall::new(
         RBC_RATES_URL.to_string(),
