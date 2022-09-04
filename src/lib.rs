2a0cb5027c161407b8fbbd37a574ae4ecb4e48c5
Mykhaylo (Misha) Ivanovych Kuzma <13275599+mishakuzma@users.noreply.github.com>
2022-09-03 18:23:11 -0600

    Comments containing planned CLI options

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,24 @@ use url::Url;
 pub struct CliInputs {
     // Users would input the command like this:
     // `rust-forexcan eur cad`
-    /// The currency you want to trade to get to_cur. Case insensitive.
+    /// Params:
+    /// from_cur(String) REQUIRED: currency you want to trade to get to_cur. Case insensitive.
     pub from_cur: String,
-    /// the currency you want to receive by giving from_cur. Case insensitive.
+    /// to_cur(String) REQUIRED: currency you want to receive by giving from_cur. Case insensitive.
     pub to_cur: String,
+    // trader(String) OPTIONAL: The organization(s) you want to trade with.
+    // Default: All
+    // Options (all case insensive)
+    // -RBC - Royal Bank of Canada
+    // -TD - TD Bank of Canada (NOT AVAILABLE)
+    // #[clap(default_value_t = String::from("All"))]
+    // pub trader: String,
+    // hard_cash(bool) OPTIONAL: Whether you want rates for ordered physical currency.
+    // Some orgs, such as TD, offer different rates if you order non-cash vs cash.
+    // Setting this to true will simulate ordering physical money.
+    // Default: False 
+    // #[clap(short = 'c', long = "hard_cash", action)]
+    // pub hard_cash: bool,
 }
 
 #[derive(Deserialize)]
@@ -117,7 +131,8 @@ const RBC_RATES_URL: &str =
 //  to be parsed.
 const TD_RATES_URL: &str = 
     "https://tool.td.com/fxcal/api/fxservice/getNonCashFeed";
-// TODO Find out where CIBC makes their rates available from.
+// NEVERMIND:Find out where CIBC makes their rates available from.
+// CIBC doesnt carry foreign currency. You have to order ahead. Probs not worth making something for them.
 // const CIBC_RATES_URL: &str = "";
 pub fn call_rbc(from_cur: String, to_cur: String) {
     let rbc_call = BankCall::new(
