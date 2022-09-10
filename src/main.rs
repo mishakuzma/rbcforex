cd84119616dfededfe9d5e408bf5de92b3d23f36
Mykhaylo (Misha) Ivanovych Kuzma <13275599+mishakuzma@users.noreply.github.com>
2022-09-10 10:29:27 -0600

    Style: Crate renamed to lowercase

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 fn main() {
-    let args = rust_forexcan::CliInputs::parse();
+    let args = rbcforex::CliInputs::parse();
 
     // rust_forexcan::call_rbc(args.from_cur, args.to_cur);
     
     // Call handler
     // Because the handler is the only part where user input matters,
     // the handler takes ownership of the inputs.
-    rust_forexcan::handle_input(args);    
+    rbcforex::handle_input(args);    
 
     // Display result
 }
